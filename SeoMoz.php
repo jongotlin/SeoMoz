4e4d1cf2d6eba857cc2af5ba42026afdaf2e61bb
Jon Gotlin <jon@jon.se>
2012-04-20 21:42:31 +0200

    First commit

---
+++ SeoMoz.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace SeoMoz;
+
+class SeoMoz
+{
+    
+    private $accessID;
+    private $secretKey;
+    
+    public function __construct($accessID, $secretKey)
+    {
+        $this->accessID = $accessID;
+        $this->secretKey = $secretKey;
+    }
+    
+    private function getSeoMozUrl($domain_name)
+    {
+        $expires = time() + 300;  // The request is good for the next 5 minutes, or 300 seconds from now.
+        $stringToSign = $this->accessID."\n".$expires;
+
+        // Get the "raw" or binary output of the hmac hash.
+        $binarySignature = hash_hmac('sha1', $stringToSign, $this->secretKey, true);
+
+        // We need to base64-encode it and then url-encode that.
+        $urlSafeSignature = urlencode(base64_encode($binarySignature));
+
+        return "http://lsapi.seomoz.com/linkscape/url-metrics/".urlencode($domain_name)."?AccessID=".$this->accessID."&Expires=".$expires."&Signature=".$urlSafeSignature;
+    }
+    
+    private function fetchData($domain_name)
+    {
+        $curl_handle = curl_init();
+
+        curl_setopt($curl_handle, CURLOPT_URL, $this->getSeoMozUrl($domain_name));
+        curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl_handle, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl_handle, CURLOPT_TIMEOUT,10);
+
+        $buffer = curl_exec($curl_handle);
+        curl_close($curl_handle);
+
+        return $buffer;
+    }
+    
+    public function getSeoMozData($domain_name)
+    {
+        return new SeoMozData($this->fetchData($domain_name));
+    }
+
+}
---
+++ SeoMozData.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace SeoMoz;
+
+class SeoMozData
+{
+    
+    private $pageAuthority;
+    private $domainAuthority;
+    private $httpStatusCode;
+    private $externalLinks;
+    
+    public function __construct($buffer)
+    {
+        if (empty($buffer)) {
+            // TODO: 
+        } else {
+            $result = json_decode($buffer);
+
+            if (isset($result->upa)) {
+                $this->setPageAuthority($result->upa);
+            }
+
+            if (isset($result->pda)) {
+                $this->setDomainAuthority($result->pda);
+            }
+            
+            if (isset($result->us)) {
+                $this->setHttpStatusCode($result->us);
+            }
+
+            if (isset($result->ueid)) {
+                $this->setHttpStatusCode($result->ueid);
+            }
+        }
+    }
+    
+    public function setPageAuthority($pageAuthority)
+    {
+        $this->pageAuthority = $pageAuthority;
+    }
+    
+    public function getPageAuthority()
+    {
+        return $this->pageAuthority;
+    }
+    
+    public function setDomainAuthority($domainAuthority)
+    {
+        $this->domainAuthority = $domainAuthority;
+    }
+    
+    public function getDomainAuthority()
+    {
+        return $this->domainAuthority;
+    }
+    
+    public function setHttpStatusCode($httpStatusCode)
+    {
+        $this->httpStatusCode = $httpStatusCode;
+    }
+    
+    public function getHttpStatusCode()
+    {
+        return $this->httpStatusCode;
+    }
+    
+    public function setExternalLinks($externalLinks)
+    {
+        $this->externalLinks = $externalLinks;
+    }
+    
+    public function getExternalLinks()
+    {
+        return $this->externalLinks; 
+    }
+}
